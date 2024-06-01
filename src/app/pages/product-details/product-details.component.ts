d5f63bdf885660d9dc8b11eda889150ef55bb6ed
JulieNonnon <julie.nonnon@gmail.com>
2024-06-01 16:41:03 +0200

    fix: product datas are now correctly fetched on product-details page, according to their respective id

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from '../../mocks/product.model';
 import { ProductService } from '../../services/product/product.service';
 
@@ -13,21 +13,26 @@ export class ProductDetailsComponent implements OnInit {
   product!: Product;
   pageTitle!: string;
 
-
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
     private productService: ProductService
-  ) { }
+  ) {}
 
-  // Configuration du ngOnInit pour la bonne récupération des éléments par id au lancement de la page product-details/:id
+// Configuration du ngOnInit pour la bonne récupération des éléments par id au lancement de la page product-details/:id
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const productId = +params['id']; // Récupération de l'ID du produit depuis l'URL
-      this.productService.getProductById(productId).subscribe((product: Product) => {
-        this.product = product; // Récupération du produit depuis le service
-        this.pageTitle = `Graines de tomate ${this.product.title}`; // Construction du titre de la page
-      });
+      this.productService.getProductById(productId).subscribe(
+        (product: Product) => {
+          this.product = product; // Récupération du produit depuis le service
+          this.pageTitle = `Graines de tomate ${this.product.title}`; // Construction du titre de la page
+        },
+        (error) => {
+          console.error('Error loading product details:', error);
+          this.router.navigateByUrl('/error404');
+        }
+      );
     });
   }
 }
-
